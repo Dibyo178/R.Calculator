34245168d410f22823681104c29701edd8609c0e
Dibyo178 <Sourov.mu@gmail.com>
2021-04-22 22:34:37 +0600

    done

---
+++ script.js
@@ -0,0 +1,28 @@
+let screen =document.getElementById('screen');
+
+let screenValue='';
+const buttons= document.querySelectorAll('button');
+
+for(item of buttons){
+    item.addEventListener('click',function(e){
+        let buttonText=e.target.innerText;
+        console.log('button clicked',buttonText);
+
+        if(buttonText=='X'){
+             buttonText='*';
+             screenValue +=buttonText
+             screen.value =screenValue;
+        }
+        else if(buttonText =='C'){
+            screenValue ='';
+            screen.value =screenValue;
+        }
+        else if(buttonText =='='){
+            screen.value =eval(screenValue);
+        }
+        else{
+            screenValue +=buttonText
+            screen.value =screenValue;
+        }
+    })
+}
